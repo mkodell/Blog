2f905307c0f466aa0a4d976cb4e85a9d6da2bad3
Meredith Odell <meredith.odell@sourcetoad.com>
2022-03-11 15:36:42 -0500

    first commit - up to date with laracasts

---
+++ app/Http/Controllers/AdminPostController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class AdminPostController extends Controller
+{
+    public function index()
+    {
+        return view('admin.posts.index', [
+            'posts' => Post::paginate(50)
+        ]);
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function store()
+    {
+        $attributes = array_merge($this->validatePost(), [
+            'user_id' => auth()->id(),
+            'thumbnail' => request()->file('thumbnail')->store('thumbnails'),
+        ]);
+
+        Post::create($attributes);
+
+        return redirect('/');
+    }
+
+    public function edit(Post $post)
+    {
+        return view('admin.posts.edit', [
+            'post' => $post
+        ]);
+    }
+
+    public function update(Post $post)
+    {
+        $attributes = $this->validatePost();
+
+        if ($attributes['thumbnail'] ?? false)
+        {
+            $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
+        }
+
+        $post->update($attributes);
+
+        return redirect()->back()->with('success', 'Post Updated!');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->back()->with('success', 'Post Deleted!');
+    }
+
+    protected function validatePost(?Post $post = null): array
+    {
+        $post ??= new Post();
+
+        return request()->validate([
+            'title' => 'required',
+            'thumbnail' => $post->exists ? ['image'] : ['required', 'image'],
+            'excerpt' => 'required',
+            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post)],
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')]
+        ]);
+    }
+}
