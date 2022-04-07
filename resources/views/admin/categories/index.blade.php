c0af6e223f3bfa813723e23b8c1e8dc929106fae
Meredith Odell <meredith.odell@sourcetoad.com>
2022-04-07 12:07:13 -0400

    removing unneeded numOfPosts variable

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -20,7 +20,7 @@
                                     <td class="px-6 py-4 whitespace-nowrap">
                                         <div class="flex items-center">
                                              <div class="text-sm font-medium text-gray-900">
-                                                 Posts: {{ $numOfPosts = $category->posts->count()}}
+                                                 Posts: {{ $category->posts->count()}}
                                             </div>
                                         </div>
                                     </td>
