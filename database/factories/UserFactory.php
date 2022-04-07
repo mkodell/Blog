b3ea072bc3785ee6b8930a561304a27160d2a58e
Meredith Odell <meredith.odell@sourcetoad.com>
2022-04-07 17:36:42 -0400

    removing todo

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,6 @@ class UserFactory extends Factory
         ];
     }
 
-    /* TODO: create an avatar feature - which can changes comment + author avatar picture. */
     /* TODO: normal user profile page - can select subscription options - change personal info (user/pass) */
 
     /**
