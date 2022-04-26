c7565da31d1f057a1af89292ab07c39565c51944
Meredith Odell <meredith.odell@sourcetoad.com>
2022-04-26 14:14:31 -0400

    adding real posts and categories to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -20,10 +21,52 @@ class DatabaseSeeder extends Seeder
             'username' => 'mkodell',
             'email' => 'meredith.odell@sourcetoad.com',
             'password' => bcrypt('password'),
-            'avatar' => 'avatars/Meredith1.jpg',
+            'avatar' => 'avatars/MeredithAvatar.jpg',
         ]);
 
-        $user = User::factory(2)->create();
-        $category = Category::factory(4)->create();
+        $user = User::factory()->create([
+            'name' => 'Jane Doe',
+            'username' => 'janed',
+            'email' => 'janedoe@example.com',
+            'password' => bcrypt('password'),
+            'avatar' => 'avatars/JaneAvatar.jpeg',
+        ]);
+
+        $category = Category::factory()->create([
+            'name' => 'Work',
+            'slug' => 'work',
+        ]);
+
+        $category = Category::factory()->create([
+            'name' => 'Home',
+            'slug' => 'home',
+        ]);
+
+        $category = Category::factory()->create([
+            'name' => 'Hobby',
+            'slug' => 'hobby',
+        ]);
+
+        $post = Post::factory()->create([
+            'user_id' => '1',
+            'category_id' => '1',
+            'slug' => 'test-1',
+            'title' => 'Test #1',
+            'thumbnail' => 'thumbnails/background.jpeg',
+            'excerpt' => 'This is the first factory generated post',
+            'body' => 'This is the first factory generated post since adding the thumbnail and status feature.',
+            'status' => 'draft'
+        ]);
+
+        $post = Post::factory()->create([
+            'user_id' => '2',
+            'category_id' => '2',
+            'slug' => 'test-2',
+            'title' => 'Test #2',
+            'thumbnail' => 'thumbnails/screenshot.png',
+            'excerpt' => 'This is the second factory generated post',
+            'body' => 'This is the second factory generated post since adding the thumbnail and status feature.',
+            'status' => 'published'
+        ]);
     }
 }
