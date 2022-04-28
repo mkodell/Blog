7d40db4e928cc2a368560f16cc672785746cb01e
Meredith Odell <meredith.odell@sourcetoad.com>
2022-04-28 15:19:37 -0400

    fixing time shown on card and initial age sorting

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(
+            'posts' => Post::latest('updated_at')->filter(
                 request(['search', 'category', 'author'])
             )->where('status', 'published')->paginate(6)->withQueryString(),
         ]);
