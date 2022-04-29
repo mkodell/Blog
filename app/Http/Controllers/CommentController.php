b88b72062947235e37b954d0f672c25d5c96b23b
Meredith Odell <meredith.odell@sourcetoad.com>
2022-04-29 14:02:36 -0400

    adding in route and validation

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,7 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+
+
 class CommentController extends Controller
 {
-    //
+    public function edit(Comment $comment) {
+        return view('comments.edit', [
+            'comment' => $comment,
+        ]);
+    }
+
+    public function update(Comment $comment) {
+        $attributes = request()->validate([
+            'body' => 'required',
+            'updated_at' => now(),
+        ]);
+
+        $comment->update($attributes);
+
+        return back()->with('success', 'Comment Updated!');
+    }
+
+    public function destroy(Comment $comment) {
+        $comment->delete();
+
+        return back()->with('success', 'Comment Deleted!');
+    }
 }
