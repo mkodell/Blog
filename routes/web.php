c3ab007f3a2d154b92006e02fb5bceb441f23423
Meredith <meredith.odell@sourcetoad.com>
2023-04-21 13:12:50 -0400

    refactor: making route names more consistent

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\AccountNewsletterController;
+use App\Http\Controllers\AdminCampaignContentController;
+use App\Http\Controllers\AdminCampaignController;
 use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\AdminPostController;
 use App\Http\Controllers\CommentController;
@@ -14,30 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:title}', [PostController::class, 'show']);
 
-Route::post('posts/{post:title}/comments', [PostCommentsController::class, 'store']);
+Route::post('posts/{post:title}/comments', [PostCommentsController::class, 'store'])->middleware('auth');
 
-Route::patch('comments/{comment:id}', [CommentController::class, 'update']);
-Route::delete('comments/{comment:id}', [CommentController::class, 'destroy']);
+Route::patch('comments/{comment:id}', [CommentController::class, 'update'])->middleware('auth');
+Route::delete('comments/{comment:id}', [CommentController::class, 'destroy'])->middleware('auth');
 
-Route::post('newsletter/guestSubscribe', [NewsletterController::class, 'guestSubscribe']);
-Route::post('newsletter/userNewSubscribe', [NewsletterController::class, 'userNewSubscribe'])->middleware('auth');
-Route::post('newsletter/userResubscribe', [NewsletterController::class, 'userResubscribe'])->middleware('auth');
-Route::patch('newsletter/unsubscribe', [NewsletterController::class, 'unsubscribe'])->middleware('auth');
-Route::get('newsletter/listCampaigns', [NewsletterController::class, 'listCampaigns'])->middleware('auth');
-Route::post('newsletter/sendCampaign/{campaign}', [NewsletterController::class, 'sendCampaign'])->middleware('auth');
-Route::delete('newsletter/deleteCampaign/{campaign}', [NewsletterController::class, 'deleteCampaign'])->middleware('auth');
-Route::post('newsletter/storeCampaign', [NewsletterController::class, 'storeCampaign'])->middleware('auth');
-Route::get('newsletter/createCampaign', [NewsletterController::class, 'createCampaign'])->middleware('auth');
-Route::put('newsletter/storeCampaignContent/{campaign}', [NewsletterController::class, 'storeCampaignContent'])->middleware('auth');
-Route::get('newsletter/editCampaign/{campaign}', [NewsletterController::class, 'editCampaign'])->middleware('auth');
+Route::post('newsletter', [NewsletterController::class, 'subscribe'])->middleware('guest');
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-
 Route::get('account/{user:username}', [AccountController::class, 'show'])->middleware('auth');
 Route::get('account/{user:username}/edit', [AccountController::class, 'edit'])->middleware('auth');
 Route::patch('account/{user:username}', [AccountController::class, 'update'])->middleware('auth');
+
+Route::post('account/newsletter', [AccountNewsletterController::class, 'newSubscribe'])->middleware('auth');
+Route::post('account/newsletter/resubscribe', [AccountNewsletterController::class, 'resubscribe'])->middleware('auth');
+Route::patch('account/newsletter', [AccountNewsletterController::class, 'unsubscribe'])->middleware('auth');
 
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
@@ -55,3 +51,11 @@ Route::delete('admin/categories/{category}', [AdminCategoryController::class, 'd
 Route::get('admin/categories/create', [AdminCategoryController::class, 'create'])->middleware('can:admin');
 Route::post('admin/categories', [AdminCategoryController::class, 'store'])->middleware('can:admin');
 
+Route::get('admin/campaigns', [AdminCampaignController::class, 'index'])->middleware('can:admin');
+Route::post('admin/campaigns/{campaign}', [AdminCampaignController::class, 'send'])->middleware('can:admin');
+Route::delete('admin/campaigns/{campaign}', [AdminCampaignController::class, 'delete'])->middleware('can:admin');
+Route::post('admin/campaigns', [AdminCampaignController::class, 'store'])->middleware('can:admin');
+Route::get('admin/campaigns/create', [AdminCampaignController::class, 'create'])->middleware('can:admin');
+Route::get('admin/campaigns/{campaign}', [AdminCampaignController::class, 'edit'])->middleware('can:admin');
+
+Route::put('admin/campaigns/{campaign}/content', [AdminCampaignContentController::class, 'store'])->middleware('can:admin');
